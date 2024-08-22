ebe9c1984dc67c38aeda81a0e3d50144cda4c883
Vladyslav <vladver1211@gmail.com>
2024-08-22 19:46:57 +0300

    first commit

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const videoMain = document.querySelector("#videoMain");
-   const canvasMain = document.querySelector("#canvasMain");
-   const pictureBtn = document.querySelector("#take-picture");
-   const image = document.querySelector("#photo");
+    const videoMain = document.querySelector("#videoMain");
+    const canvasMain = document.querySelector("#canvasMain");
+    const pictureBtn = document.querySelector("#take-picture");
+    const showVideoBtn = document.querySelector("#show-video");
+    const image = document.querySelector("#photo");
+    const predictionText = document.querySelector("#prediction-text");
+    const changeCamera = document.querySelector("#change-camera");
 
-    //environment - rear , user - front camera
+    let camera = "user";
+
+    const classifier = ml5.imageClassifier("MobileNet");
+
+    changeCamera.addEventListener("change-camera", () => {
+        if (camera === "user") {
+            camera = "environment";
+        }else if (camera === "environment") {
+            camera = "user";
+        }
+    });
+
+    //enviroment - rear , user - front camera
     const constaints = {
-        video: { facingMode: "user"  },
+        video: { facingMode: camera  },
         audio: false
     }
 
+    console.log(camera)
     navigator.mediaDevices.getUserMedia(constaints)
         .then(steam => {
              videoMain.srcObject = steam;
@@ -31,7 +47,38 @@ document.addEventListener("DOMContentLoaded", () => {
         
         // Set the img element source to the captured photo
         image.src = dataURL;
-        });
 
+        showImg();
+        classifier.classify(image, gotResult);
+        
+    });
+
+    function showImg() {
+        // Update video and img
+        videoMain.style.visibility = 'hidden';
+        videoMain.style.display = 'none';
+        image.style.visibility = 'visible';
+        image.style.display = 'block';
+
+        showVideoBtn.disabled = false;
+    }
+
+    function gotResult(result){
+        console.log(result);
+        predictionText.innerText = `Prediction: ${result[0].label}`
+        predictionText.classList.add('text-animate');
+    }
+
+    showVideoBtn.addEventListener('click', () => {
+        videoMain.style.visibility = 'visible';
+        videoMain.style.display = 'block';
+        image.style.visibility = 'hidden';
+        image.style.display = 'none';
+
+        predictionText.innerText = ``
+        predictionText.classList.remove('text-animate');
+
+        showVideoBtn.disabled = true;
+    });
 
 });
